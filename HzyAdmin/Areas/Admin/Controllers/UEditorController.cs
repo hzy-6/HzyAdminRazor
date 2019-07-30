e51712e3d3870806aeb8ba7e6c2818d953398ab9
hzy <1396510655@qq.com>
2019-07-30 11:14:38 +0800

    editor 上传 oss 或者外部资源 例子

--- HzyAdmin/Areas/Admin/Controllers/UEditorController.cs
+++ HzyAdmin/Areas/Admin/Controllers/UEditorController.cs
@@ -20,6 +20,54 @@ namespace HzyAdmin.Areas.Admin.Controllers
         {
             var response = _ueditorService.UploadAndGetResponse(HttpContext);
             return Content(response.Result, response.ContentType);
+
+
+            #region 将资源上传 oss 或者其他服务器 仅供参考
+
+            //if (HttpContext.Request.Query["action"].ToStr() == "config")
+            //{
+            //    var response = _ueditorService.UploadAndGetResponse(HttpContext);
+            //    return Content(response.Result, response.ContentType);
+            //}
+
+            ////OSS上传
+            //var _Files = HttpContext.Request.Form.Files;
+            //var _Url = string.Empty;
+            //IFormFile _IFormFile = null;
+            //if (_Files.Count > 0)
+            //{
+            //    foreach (var item in _Files)
+            //    {
+            //        _IFormFile = item;
+            //        var ExtensionName = Path.GetExtension(item.FileName).ToLower().Trim();//获取后缀名
+            //        _Url += $"Editor/{Guid.NewGuid()}{ExtensionName}";
+            //        item.OpenReadStream().UploadFile(_Url);
+            //    }
+            //}
+
+            //if (_IFormFile != null)
+            //{
+            //    return Json(new
+            //    {
+            //        original = _IFormFile.FileName,
+            //        state = "SUCCESS",
+            //        title = _IFormFile.FileName,
+            //        url = AppConfig.Configs.OSSDomain + _Url
+            //    });
+            //}
+
+            //return Json(new
+            //{
+            //    original = "",
+            //    state = "Error",
+            //    title = "",
+            //    url = ""
+            //});
+
+            #endregion
+
+
+
         }
 
     }
